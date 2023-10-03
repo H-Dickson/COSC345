f9949ffaaf1fd5dcdd56c3c6dd9e262f4d21a243
H-Dickson <139689444+H-Dickson@users.noreply.github.com>
2023-10-03 19:47:44 +1300

    Console for JS

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -9,6 +9,39 @@
 
 using namespace ultralight;
 
+inline std::string ToUTF8(const String& str) {
+    String8 utf8 = str.utf8();
+    return std::string(utf8.data(), utf8.length());
+}
+
+inline const char* Stringify(MessageSource source) {
+    switch (source) {
+    case kMessageSource_XML: return "XML";
+    case kMessageSource_JS: return "JS";
+    case kMessageSource_Network: return "Network";
+    case kMessageSource_ConsoleAPI: return "ConsoleAPI";
+    case kMessageSource_Storage: return "Storage";
+    case kMessageSource_AppCache: return "AppCache";
+    case kMessageSource_Rendering: return "Rendering";
+    case kMessageSource_CSS: return "CSS";
+    case kMessageSource_Security: return "Security";
+    case kMessageSource_ContentBlocker: return "ContentBlocker";
+    case kMessageSource_Other: return "Other";
+    default: return "";
+    }
+}
+
+inline const char* Stringify(MessageLevel level) {
+    switch (level) {
+    case kMessageLevel_Log: return "Log";
+    case kMessageLevel_Warning: return "Warning";
+    case kMessageLevel_Error: return "Error";
+    case kMessageLevel_Debug: return "Debug";
+    case kMessageLevel_Info: return "Info";
+    default: return "";
+    }
+}
+
 MyApp::MyApp() {
 
 
@@ -127,6 +160,26 @@ void MyApp::OnChangeCursor(ultralight::View* caller,
 void MyApp::OnChangeTitle(ultralight::View* caller,
     const String& title) {
     window_->SetTitle(title.utf8().data());
+}
+
+void MyApp::OnAddConsoleMessage(View* caller,
+    MessageSource source,
+    MessageLevel level,
+    const String& message,
+    uint32_t line_number,
+    uint32_t column_number,
+    const String& source_id) {
+
+    std::cout << "[Console]: [" << Stringify(source) << "] ["
+        << Stringify(level) << "] " << ToUTF8(message);
+
+    if (source == kMessageSource_JS) {
+        std::cout << " (" << ToUTF8(source_id) << " @ line " << line_number
+            << ", col " << column_number << ")";
+    }
+
+    std::cout << std::endl;
+
 }
 
 // This function will be called from JavaScript
